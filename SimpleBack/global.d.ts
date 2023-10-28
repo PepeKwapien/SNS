7c519a57645835a1228d509d4da164a0f7b7ddec
PepeKwapien <pppkwapien@gmail.com>
2023-10-28 16:00:49 +0200

    Test connection to mongo

--- SimpleBack/global.d.ts
+++ SimpleBack/global.d.ts
@@ -2,5 +2,6 @@ namespace NodeJS {
     interface ProcessEnv {
         PORT: number;
         HOSTNAME: string;
+        MONGODB_URL: string;
     }
 }
--- SimpleBack/index.ts
+++ SimpleBack/index.ts
@@ -1,7 +1,41 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+    title: { type: String, require: true },
+    description: String,
+    dueDate: { type: Date, require: true },
+    reminderTime: Date
+});
+
+const taskModel = mongoose.model('Task', taskSchema);
 
 dotenv.config();
+
+const mongoUri: string = process.env.MONGODB_URL;
+
+mongoose
+    .connect(mongoUri)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log(err));
+
+const dbConnection = mongoose.connection;
+
+dbConnection.on('error', (err) => {
+    console.error(`MongoDB connection error: ${err}`);
+});
+
+dbConnection.once('open', async () => {
+    console.log('Connected to MongoDB');
+    console.log(await dbConnection.db.collections());
+
+    const task = new taskModel({ title: 'Connecting Mongo', description: 'Trying it out', dueDate: Date.now() });
+    task.save().then(
+        () => console.log('One entry added'),
+        (err) => console.log(err)
+    );
+});
 
 const hostname: string = process.env.HOSTNAME;
 const port: number = process.env.PORT;
